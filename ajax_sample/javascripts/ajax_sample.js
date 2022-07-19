ed581ae9646faeb438af9b41466e4b6bb5bbae8b
camoes <cam@camoes.fr>
2022-07-19 22:07:46 +0100

    refonte du code js

--- ajax_sample/javascripts/ajax_sample.js
+++ ajax_sample/javascripts/ajax_sample.js
@@ -1,28 +1,26 @@
 let number = 0;
-
 const button = document.getElementById('btn');
 
 function getData() {
-  const request = new XMLHttpRequest();
+  button.addEventListener('click', e => {
+    const request = new XMLHttpRequest();
     request.onreadystatechange = function() {
-      if (request.readyState == 4 && request.status == 200) {
-          const videoArea = document.getElementById("video");
-          const titleArea = document.getElementById("title");
-          const contentArea = document.getElementById("content");
+      const videoArea = document.getElementById("video");
+      const titleArea = document.getElementById("title");
+      const contentArea = document.getElementById("content");
+      const button = document.getElementById('btn');
+      if (request.readyState == 4) {
+        if(request.status == 200) {
           titleArea.innerHTML = request.response[number].title;
           contentArea.innerHTML = request.response[number].content;
           videoArea.innerHTML = request.response[number].url;
           number == 2 ? number = 0 : number++;
+        }
       }
     }
-}
-
-function changeVideo() {
-  button.addEventListener('click', e => {
-    getData()
     request.open("GET", "ajax.json");
     request.responseType = "json";
     request.send(null);
   })
 }
-window.onload = changeVideo;
+window.onload = getData;
